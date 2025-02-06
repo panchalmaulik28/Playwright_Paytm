4a37e6a9d5adaeee1780c50d57bbbb85c30c82e9
panchalmauik28 <panchalmaulik28@gmail.com>
2025-02-06 09:03:49 +0530

    Issue resolved
    Issue resolved

--- src/main/java/com/pages/FlightPage.java
+++ src/main/java/com/pages/FlightPage.java
@@ -4,12 +4,13 @@ import com.factory.BrowserFactory;
 
 public class FlightPage extends BrowserFactory {
 
-	String tripWayRadio = "//div[@class=\"_3Vm7i\"]/span/div/label";
-	String fromDD = "//div[@class=\"_2TAdi\"]/div[1]/span[2]";
-	String toDD = "//div[@class=\"_2TAdi\"]/div[2]/span[2]";
+	String tripWayRadio = "//div[@class='_3Vm7i']/span/div/label";
+	String fromDD = "//div[@class='_2TAdi']/div[1]/span[2]";
+	String toDD = "//div[@class='_2TAdi']/div[2]/span[2]";
 	String sourceTxt = "//input[@placeholder='Source']";
 	String destinationTxt = "//input[@placeholder='Destination']";
-	String placeList = "//div[@class=\"_3lt44\"]";
+	String placeList = "//div[@class='_3lt44']";
+	String placeName = "//div[@class='Egrgh']/div";
 	String calender = "//span[@id='departureDate']";
 	String searchFlightBtn = "//button[@id='flightSearch']";
 
@@ -21,19 +22,20 @@ public class FlightPage extends BrowserFactory {
 	public void searchSourceDestination(String source, String destination) {
 		page.locator(fromDD).click();
 		page.locator(sourceTxt).fill(source);
+		page.waitForSelector(placeList);
 		int placeCount = page.locator(placeList).count();
 		for (int i = 0; i <= placeCount; i++) {
 			if(page.locator(placeList).allTextContents().contains(source)) {
-				
-				page.locator(placeList).nth(i).click();	
+				page.locator(placeName).nth(i).click();	
 				break;
 			}
 		}
 		page.locator(toDD).click();
 		page.locator(destinationTxt).fill(destination);
+		page.waitForSelector(placeList);
 		for (int i = 0; i <= placeCount; i++) {
 			if(page.locator(placeList).allTextContents().contains(destination)) {
-				page.locator(placeList).nth(i).click();	
+				page.locator(placeName).nth(i).click();	
 				break;
 			}
 		}
